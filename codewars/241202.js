64553e3e686ded3ea38d909f15db3fc699214841
soeunSim <629943@naver.com>
2024-12-04 08:47:20 +0900

    241202 Are You Playing Banjo?--finish

--- codewars/241202.js
+++ codewars/241202.js
@@ -1,4 +1,10 @@
 function areYouPlayingBanjo(name) {
-    // Implement me
-    return name;
+  // Implement me
+  if(name.charAt(0) === "R" || name.charAt(0) === "r"){
+    return `${name} plays banjo`
   }
+  
+  return `${name} does not play banjo`;
+}
+
+areYouPlayingBanjo('Dorooo')
