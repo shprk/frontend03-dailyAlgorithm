a5961b1b652019470f60be70096fa0d7f5e01d99
mpnisck <mpnisck@gmail.com>
2024-12-07 09:42:11 +0900

    zeroFuel default

--- CodeWars_Yeonju/const zeroFuel = (distanceToPump, mpg, f.js
+++ CodeWars_Yeonju/const zeroFuel = (distanceToPump, mpg, f.js
@@ -1,8 +1,8 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
   // TODO 마일, 갤런, 
-  if (distanceToPump / mpg === fuelLeft) { // 예제문제 집착하지말기
+  if (distanceToPump / mpg <= fuelLeft) { // 예제문제 집착하지말기
     return true
   } else {
     return false
-  }
+  } 
 };
