bd4a13d2798c2155a88ef1375e45690b6a6cacfb
soeunSim <629943@naver.com>
2024-12-08 18:45:54 +0900

    "1208 모던js 예제 if문 <-> switch 문"

---
+++ codewars/241207-night.js
@@ -0,0 +1,14 @@
+function reverse(string){
+    //주어진 문자열의 단어를 반전시키는 함수를 작성해야 합니다. 
+    //단어는 항상 하나의 공백으로 구분됩니다. 
+    //입력에 후행 공백이 있을 수 있으므로 불필요한 공백도 무시해야 합니다. 
+    //예제(입력 --> 출력)
+
+    //문자열을 배열로 바꾸는 법 조사하기
+    //1) String.prototype.split() : 스트링 프로토타입 메서드 split
+    //2) [...string] : 스프레드 연산자 (ES6부터 추가)
+    // 3) Array.from(string) : Array 객체의 정적 메서드
+
+    //Array.prototype.reverse() 매서드 조사하기
+    
+  }
