e107c11e931bbf560676e29921f7179ddda96fb8
seonghoon <shee5032@naver.com>
2024-12-14 22:41:16 +0900

    feat: modified 241214.js

--- Codewars/241214.js
+++ Codewars/241214.js
@@ -11,3 +11,23 @@ function getCount(str) {
   }
   return count;
 }
+
+// Disemvowel Trolls
+function disemvowel(str) {
+  let vowelDelStr = "";
+  for(let i = 0; i < str.length; i++){
+    if(str[i] === "a" || str[i] === "A"){
+      continue;
+    }else if(str[i] === "i" || str[i] === "I"){
+      continue;
+    }else if(str[i] === "e" || str[i] === "E"){
+      continue;
+    }else if(str[i] === "o" || str[i] === "O"){
+      continue;
+    }else if(str[i] === "u" || str[i] === "U"){
+      continue;
+    }
+    vowelDelStr += str[i];
+  }
+  return vowelDelStr;
+}
